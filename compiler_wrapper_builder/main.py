befa8dc777803170f3f467b24ef4f98f4b25ac08
Pavel Ershov <pershov@game-insight.com>
2020-05-28 10:37:18 +0300

    Wrapper improvement

--- compiler_wrapper_builder/main.py
+++ compiler_wrapper_builder/main.py
@@ -5,6 +5,7 @@ import argparse
 import subprocess
 import os
 import os.path
+import multiprocessing
 
 
 def build_bash_wrapper(result_folder, compiler_path, compiler_args, use_distcc, use_ccache, distcc_path, ccache_path) -> str:
@@ -49,156 +50,105 @@ def build_bash_wrapper(result_folder, compiler_path, compiler_args, use_distcc, 
         with open(file_path, "w") as file:
             file.write(text)
 
+        os.system("chmod u+x {}".format(file_path))
+
         return file_path        
     else:
         return None
 
 
 def build_c_wrapper(result_folder, compiler_path, compiler_args: str, use_distcc, use_ccache, distcc_path, ccache_path) -> str:
-    compiler_path = None
+    compilator_for_c_code = None
 
     clang_out = subprocess.run(["which", "clang"], capture_output=True)
     if (clang_out.returncode == 0) and (len(clang_out.stdout) > 1):
-        compiler_path = clang_out.stdout.decode("utf-8").rstrip("\n")
+        compilator_for_c_code = clang_out.stdout.decode("utf-8").rstrip("\n")
     else:
         gcc_out = subprocess.run(["which", "gcc"], capture_output=True)
         if (gcc_out.returncode == 0) and (len(gcc_out.stdout) > 1):
-            compiler_path = gcc_out.stdout.decode("utf-8").rstrip("\n")
+            compilator_for_c_code = gcc_out.stdout.decode("utf-8").rstrip("\n")
 
-    if not compiler_path:
+    if not compilator_for_c_code:
         return None
     
+    args = ""
+    prefixes_count = 0
+    env = ""
+
     if use_ccache and use_distcc:
-        args = ""
-        prefixes_count = 0
-
-        args += "args_new[index++] = \"{}\";\n".format(ccache_path)
+        env = "          putenv(\"CCACHE_PREFIX={}\");\n".format(distcc_path)
+        args += "          args_new[index++] = \"{}\";\n".format(ccache_path)
         prefixes_count += 1
-        
-        if compiler_path:
-            args += "args_new[index++] = \"{}\";\n".format(compiler_path)
-            prefixes_count += 1
-
-            compiler_args_array = compiler_args.split(" ")
-            for param in compiler_args_array:
-                prefixes_count += 1
-                args += "args_new[index++] = \"{}\";\n".format(param)
-
-        text = ""\
-        "#include <stdio.h>                                 \n"\
-        "#include <stdlib.h>                                \n"\
-        "#include <unistd.h>                                \n"\
-        "#include <errno.h>                                \n"\
-        "int main(int argc, char* argv[]){{                  \n"\
-        "    putenv(\"CCACHE_PREFIX={distcc_path}\");          \n"\
-        "    const int prefixes = {prefixes_count};         \n"\
-        "    char** args_new = (char**)malloc(sizeof(char*) * (argc + prefixes + 1)); \n"\
-        "    int index = 0;                                  \n"\
-        "    {args}                                          \n"\
-        "    for(int j = 1; j < argc; j++){{              \n"\
-        "         args_new[index++] = argv[j];                     \n"\
-        "    }}                                               \n"\
-        "    args_new[index] = NULL;                     \n"\
-        "    if (execvp(args_new[0], args_new) != 0){{        \n"\
-        "         return -1;                                 \n"\
-        "    }}                                               \n"\
-        "    return errno;                                   \n"\
-        "}};\n"\
-        .format( distcc_path=distcc_path, 
-                 prefixes_count=prefixes_count, 
-                 args=args) 
     elif use_ccache:
-        args = ""
-        prefixes_count = 0
-
-        args += "args_new[index++] = \"{}\";\n".format(ccache_path)
+        args += "          args_new[index++] = \"{}\";\n".format(ccache_path)
         prefixes_count += 1
-        
-        if compiler_path:
-            args += "args_new[index++] = \"{}\";\n".format(compiler_path)
-            prefixes_count += 1
-
-            compiler_args_array = compiler_args.split(" ")
-            for param in compiler_args_array:
-                prefixes_count += 1
-                args += "args_new[index++] = \"{}\";\n".format(param)
-
-        text = ""\
-        "#include <stdio.h>                                 \n"\
-        "#include <stdlib.h>                                \n"\
-        "#include <unistd.h>                                \n"\
-        "#include <errno.h>                                \n"\
-        "int main(int argc, char* argv[]){{                  \n"\
-        "    const int prefixes = {prefixes_count};         \n"\
-        "    char** args_new = (char**)malloc(sizeof(char*) * (argc + prefixes + 1)); \n"\
-        "    int index = 0;                                  \n"\
-        "    {args}                                          \n"\
-        "    for(int j = 1; j < argc; j++){{              \n"\
-        "         args_new[index++] = argv[j];                     \n"\
-        "    }}                                               \n"\
-        "    args_new[index] = NULL;                     \n"\
-        "    if (execvp(args_new[0], args_new) != 0){{        \n"\
-        "         return -1;                                 \n"\
-        "    }}                                               \n"\
-        "    return errno;                                   \n"\
-        "}};\n"\
-        .format( prefixes_count=prefixes_count, 
-                 args=args)
     elif use_distcc:
-        args = ""
-        prefixes_count = 0
-
-        args += "args_new[index++] = \"{}\";\n".format(distcc_path)
+        args += "          args_new[index++] = \"{}\";\n".format(distcc_path)
         prefixes_count += 1
-        
-        if compiler_path:
-            args += "args_new[index++] = \"{}\";\n".format(compiler_path)
-            prefixes_count += 1
+    else:
+        pass
 
+    if compiler_path:
+        args += "          args_new[index++] = \"{}\";\n".format(compiler_path)
+        prefixes_count += 1
+
+        if compiler_args:
             compiler_args_array = compiler_args.split(" ")
             for param in compiler_args_array:
                 prefixes_count += 1
-                args += "args_new[index++] = \"{}\";\n".format(param)
+                args += "          args_new[index++] = \"{}\";\n".format(param)
 
-        text = ""\
-        "#include <stdio.h>                                 \n"\
-        "#include <stdlib.h>                                \n"\
-        "#include <unistd.h>                                \n"\
-        "#include <errno.h>                                \n"\
-        "int main(int argc, char* argv[]){{                  \n"\
-        "    const int prefixes = {prefixes_count};         \n"\
-        "    char** args_new = (char**)malloc(sizeof(char*) * (argc + prefixes + 1)); \n"\
-        "    int index = 0;                                  \n"\
-        "    {args}                                          \n"\
-        "    for(int j = 1; j < argc; j++){{              \n"\
-        "         args_new[index++] = argv[j];                     \n"\
-        "    }}                                               \n"\
-        "    args_new[index] = NULL;                     \n"\
-        "    if (execvp(args_new[0], args_new) != 0){{        \n"\
-        "         return -1;                                 \n"\
-        "    }}                                               \n"\
-        "    return errno;                                   \n"\
-        "}};\n"\
-        .format( prefixes_count=prefixes_count, 
-                 args=args)
-                
-    else:
-        # TODO: ???
-        text = ""
+    text = ""\
+    "#include <stdio.h>                                             \n"\
+    "#include <stdlib.h>                                            \n"\
+    "#include <unistd.h>                                            \n"\
+    "#include <errno.h>                                             \n"\
+    "int main(int argc, char* argv[]){{                             \n"\
+    "     if(fork() == 0){{                                         \n"\
+    "{env}"\
+    "          const int prefixes = {prefixes_count};               \n"\
+    "          char** args_new = (char**)malloc(sizeof(char*) * (argc + prefixes + 1)); \n"\
+    "          int index = 0;                                       \n"\
+    "{args}"\
+    "          for(int j = 1; j < argc; j++){{                      \n"\
+    "              args_new[index++] = argv[j];                     \n"\
+    "          }}                                                   \n"\
+    "          args_new[index] = NULL;                              \n"\
+    "          if (execvp(args_new[0], args_new) != -1){{           \n"\
+    "               exit(0);                                        \n"\
+    "               return 0;                                       \n"\
+    "          }}else{{                                             \n"\
+    "               exit(errno);                                    \n"\
+    "               return errno;                                   \n"\
+    "          }}                                                   \n"\
+    "    }}                                                         \n"\
+    "    int status;                                                \n"\
+    "    wait(&status);                                             \n"\
+    "    return WEXITSTATUS(status);                                \n"\
+    "}}                                                             \n"\
+    .format(env=env,
+            prefixes_count=prefixes_count, 
+            args=args)
         
+    # print(text)
+
     if result_folder:
         if not os.path.exists(result_folder):
             os.makedirs(result_folder)
 
         wrapper_file_path = os.path.join(result_folder, "compiler_wrapper")
 
-        with open("/tmp/main.c", "w") as f:
+        main_file_path = os.path.join(result_folder, "main.c")
+
+        with open(main_file_path, "w") as f:
             f.write(text)
 
-        compiler_out = subprocess.run([compiler_path, "-O2", "/tmp/main.c", "-o", wrapper_file_path], capture_output=True)
+        compiler_out = subprocess.run([compilator_for_c_code, "-O2", main_file_path, "-o", wrapper_file_path], capture_output=True)
         if compiler_out.returncode != 0:
             print(compiler_out.stderr)
             return None
+
+        os.system("chmod u+x {}".format(wrapper_file_path))
 
         return wrapper_file_path
     else:
@@ -269,12 +219,17 @@ def main():
     else:
         use_ccache = False
 
-    result_file = build_c_wrapper(args.result_folder, args.compiler, args.compiler_args, use_distcc, use_ccache, distcc_path, ccache_path)
-    if not result_file:
-        result_file = build_bash_wrapper(args.result_folder, args.compiler, args.compiler_args, use_distcc, use_ccache, distcc_path, ccache_path)
-    
-    print(result_file)   
-
+    if args.jobs:
+        if use_distcc:
+            os.system("distcc -j")
+        else:
+            print(multiprocessing.cpu_count())
+    else:
+        result_file = build_c_wrapper(args.result_folder, args.compiler, args.compiler_args, use_distcc, use_ccache, distcc_path, ccache_path)
+        if not result_file:
+            result_file = build_bash_wrapper(args.result_folder, args.compiler, args.compiler_args, use_distcc, use_ccache, distcc_path, ccache_path)
+        
+        print(result_file)   
 
 if __name__ == "__main__":
     main()
--- compiler_wrapper_builder/test.sh
+++ compiler_wrapper_builder/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 main.py --compiler "clang" --compiler_args="-lstdc++" --use_distcc --use_ccache --result_folder res
+# python3 main.py --compiler "clang" --compiler_args="-lstdc++" --use_distcc --use_ccache --result_folder res
 # python3 main.py --compiler "clang" --compiler_args="-lstdc++" --use_ccache --result_folder res
 # python3 main.py --compiler "clang" --compiler_args="-lstdc++" --use_distcc --result_folder res
 # python3 main.py --compiler "clang" --compiler_args="-lstdc++" --result_folder res
@@ -8,6 +8,8 @@ python3 main.py --compiler "clang" --compiler_args="-lstdc++" --use_distcc --use
 # python3 main.py --compiler "clang"  --use_ccache --result_folder res
 # python3 main.py --compiler "clang"  --use_distcc --result_folder res
 # python3 main.py --compiler "clang" --result_folder res
-# python3 main.py --jobs
+# python3 main.py --result_folder res
+python3 main.py --jobs
+python3 main.py --jobs --use_distcc
 
 # python3 main.py -h
